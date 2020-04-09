a82877e7d2431902ebda854740920069ff74e025
cliu192 <63021964+cliu192@users.noreply.github.com>
2020-04-08 23:04:13 -0600

    Added comment for the format of the additional description for the Gear constructor.

--- climbing-gear-lib/Gear.h
+++ climbing-gear-lib/Gear.h
@@ -5,6 +5,8 @@
 class Gear
 {
 public:
+	// if populating the additionalDescription in order to create a miscellaneous gear instance, 
+	// use the format ":: [attribute1] : [attribute1 value] :: [attribute2] : [attribute2 value]", etc.
 	Gear(std::string manufacturerName, std::string productName, std::string additionalDescription = "");
 	virtual ~Gear() = default;
 	std::string getDescription();
