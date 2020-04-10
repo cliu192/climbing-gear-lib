18e972d00ae76f320047c3da7de0a3e0ed09bffd
cliu192 <63021964+cliu192@users.noreply.github.com>
2020-04-10 01:17:57 -0600

    Implemented GearInventory to get all unit tests passing.

--- climbing-gear-lib/GearInventory.cpp
+++ climbing-gear-lib/GearInventory.cpp
@@ -6,13 +6,42 @@ GearInventory::GearInventory()
 
 void GearInventory::add(std::unique_ptr<Gear> gearToAdd)
 {
+	auto it = m_inventory.find(gearToAdd);
+
+	if (it == m_inventory.end()) // gear to be added isn't already in inventory
+	{
+		m_inventory.insert(std::make_pair(std::move(gearToAdd), 1));
+	}
+	else // gear is already in inventory, just increment count
+	{
+		++(it->second);
+	}
 }
 
-void GearInventory::remove(std::unique_ptr<Gear> gearToRemove)
+void GearInventory::remove(std::unique_ptr<Gear> gearToRemove) // pass in by reference?
 {
+	auto it = m_inventory.find(gearToRemove);
+
+	if (it->second > 1) // multiple copies of gear exist, just remove one
+	{
+		--(it->second);
+	}
+	else // only one copy exists, remove entire inventory entry
+	{
+		m_inventory.erase(it);
+	}
 }
 
-std::vector<std::string> GearInventory::getSummary()
+std::vector<std::string> GearInventory::getSummary() // TODO make more efficient
 {
-	return std::vector<std::string>();
+	std::vector <std::string> totalSummary; 
+
+	for (auto& gear : m_inventory)
+	{
+		auto gearSummary = "(" + std::to_string(gear.second) + ") ";
+		gearSummary += gear.first->getDescription();
+		totalSummary.push_back(gearSummary);
+	}
+
+	return totalSummary;
 }
--- climbing-gear-lib/GearInventory.h
+++ climbing-gear-lib/GearInventory.h
@@ -13,6 +13,25 @@ public:
 	void remove(std::unique_ptr<Gear> gearToRemove);
 	std::vector<std::string> getSummary();
 private:
-	//std::map<std::unique_ptr<Gear>, int> m_inventory;
+	struct m_comparator
+	{
+		bool operator()(const std::unique_ptr<Gear>& a, const std::unique_ptr<Gear>& b) const
+		{
+			bool returnValue;
+			auto aDescription = a->getDescription();
+			auto bDescription = b->getDescription();
+
+			if (aDescription == bDescription)
+			{
+				returnValue = false;
+			}
+			else
+			{
+				returnValue = aDescription > bDescription;
+			}
+			return returnValue;
+		}
+	};
+	std::map <std::unique_ptr<Gear>, int, m_comparator> m_inventory;
 };
 
